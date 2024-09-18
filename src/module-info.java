bf268da03ae0c52c98e511cd1edeac910baee727
gexgener1l <77413669+gexgener1l@users.noreply.github.com>
2024-09-18 15:16:49 +0300

    change color gamma

---
+++ src/module-info.java
@@ -0,0 +1,11 @@
+module laba1 {
+    requires javafx.controls;
+    requires javafx.fxml;
+    requires javafx.graphics;
+    requires javafx.base;
+    requires javafx.web;
+    requires javafx.media;
+    requires javafx.swing;
+
+    exports demo.parallel;
+}
